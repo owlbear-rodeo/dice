0c3f2fc7f3fad133fe611458d2f2a08da5678891
Mitchell McCaffrey <mitchemmc@gmail.com>
2022-10-23 17:03:40 +1100

    Add gradient overlay height control

--- src/controls/GradientOverlay.tsx
+++ src/controls/GradientOverlay.tsx
@@ -1,13 +1,15 @@
 import { styled } from "@mui/material/styles";
 
 export const GradientOverlay = styled("div", {
-  shouldForwardProp: (prop) => prop !== "top",
-})<{ top?: boolean }>(({ top }) => ({
+  shouldForwardProp: (prop) => prop !== "top" && prop !== "height",
+})<{ top?: boolean; height?: number }>(({ top, height }) => ({
   background: `linear-gradient(to ${
     top ? "bottom" : "top"
-  }, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0) 75px), linear-gradient(to ${
+  }, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0) ${
+    (height || 100) * 0.75
+  }px), linear-gradient(to ${
     top ? "bottom" : "top"
-  }, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0) 100px)`,
+  }, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0) ${height || 100}px)`,
   position: "absolute",
   zIndex: 0,
   top: 0,
