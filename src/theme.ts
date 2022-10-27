b9c1688bd93fb896fea839ad5e44463a930030a0
Mitchell McCaffrey <mitchemmc@gmail.com>
2022-10-27 14:34:12 +1100

    Adjust theme dialog background

--- src/theme.ts
+++ src/theme.ts
@@ -35,7 +35,7 @@ export const themeOptions: ThemeOptions = {
       styleOverrides: {
         paper: {
           backgroundImage:
-            "linear-gradient(rgba(255, 255, 255, 0.05), rgba(255, 255, 255, 0.05))",
+            "linear-gradient(rgba(255, 255, 255, 0.07), rgba(255, 255, 255, 0.07))",
         },
       },
     },
