4b8c8489763a25ae5568e9b6106454ca05318eb4
Mitchell McCaffrey <mitchemmc@gmail.com>
2022-10-27 18:45:34 +1100

    Add interpolation to player dice

--- src/plugin/PlayerDice.tsx
+++ src/plugin/PlayerDice.tsx
@@ -1,9 +1,14 @@
 import { Player } from "owlbear-rodeo-sdk";
-import { useRef } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import * as THREE from "three";
+import { RenderCallback, useFrame, useThree } from "@react-three/fiber";
+
 import { Dice } from "../dice/Dice";
 import { getDieFromDice } from "../helpers/getDieFromDice";
 import { useDiceInteraction } from "./useDiceInteraction";
 import { usePlayerDice } from "./usePlayerDice";
+import { Die } from "../types/Die";
+import { DiceTransform } from "../types/DiceTransform";
 
 export function PlayerDice({ player }: { player?: Player }) {
   const { diceRoll, rollTransforms } = usePlayerDice(player);
@@ -15,20 +20,113 @@ export function PlayerDice({ player }: { player?: Player }) {
   return (
     <group ref={parentRef}>
       {diceRoll &&
-        getDieFromDice(diceRoll).map((die) => {
-          const transform = rollTransforms?.[die.id];
-          const p = transform?.position;
-          const r = transform?.rotation;
-          return (
-            <Dice
-              userData={{ dieId: die.id }}
-              key={die.id}
-              die={die}
-              position={p ? [p.x, p.y, p.z] : [0, -100, 0]} // Put dice out of frame to start with
-              quaternion={r && [r.x, r.y, r.z, r.w]}
-            />
-          );
-        })}
+        getDieFromDice(diceRoll).map((die) => (
+          <PossibleInterpolatedDice
+            key={die.id}
+            die={die}
+            transform={rollTransforms?.[die.id]}
+          />
+        ))}
+    </group>
+  );
+}
+
+/**
+ * Create an interpolated dice if the initial transform state is undefined
+ */
+function PossibleInterpolatedDice({
+  die,
+  transform,
+}: {
+  die: Die;
+  transform: DiceTransform | undefined;
+}) {
+  const [initialTransform] = useState(transform);
+
+  if (initialTransform) {
+    const p = initialTransform.position;
+    const r = initialTransform.rotation;
+    return (
+      <Dice
+        userData={{ dieId: die.id }}
+        key={die.id}
+        die={die}
+        position={[p.x, p.y, p.z]}
+        quaternion={[r.x, r.y, r.z, r.w]}
+      />
+    );
+  } else {
+    return <InterpolatedDice die={die} transform={transform} />;
+  }
+}
+
+const INTERP_TIME = 500;
+/**
+ * Smooth out the transition between the dice interaction
+ * and the final dice transform by interpolating to the final value
+ * over `INTERP_TIME` ms.
+ */
+function InterpolatedDice({
+  die,
+  transform,
+}: {
+  die: Die;
+  transform: DiceTransform | undefined;
+}) {
+  const { invalidate } = useThree();
+
+  const groupRef = useRef<THREE.Group>(null);
+
+  const transformTimeRef = useRef(performance.now());
+  useEffect(() => {
+    transformTimeRef.current = performance.now();
+  }, [transform]);
+
+  const position = useMemo(() => {
+    if (transform) {
+      return new THREE.Vector3(
+        transform.position.x,
+        transform.position.y,
+        transform.position.z
+      );
+    } else {
+      return null;
+    }
+  }, [transform]);
+
+  const quaternion = useMemo(() => {
+    if (transform) {
+      return new THREE.Quaternion(
+        transform.rotation.x,
+        transform.rotation.y,
+        transform.rotation.z,
+        transform.rotation.w
+      );
+    } else {
+      return null;
+    }
+  }, [transform]);
+
+  const interpolate = useCallback<RenderCallback>(() => {
+    const group = groupRef.current;
+    if (position && quaternion && group) {
+      const deltaTime = performance.now() - transformTimeRef.current;
+      const alpha = Math.min(1, deltaTime / INTERP_TIME);
+      group.position.lerp(position, alpha);
+      group.quaternion.slerp(quaternion, alpha);
+
+      // Continue the interpolation as long as we need
+      if (deltaTime <= INTERP_TIME) {
+        invalidate();
+      }
+    }
+  }, [position, quaternion]);
+
+  useFrame(interpolate);
+
+  return (
+    <group userData={{ dieId: die.id }} ref={groupRef}>
+      <Dice die={die} />
     </group>
   );
 }
