06c17c59111c98d8e8a58a408973cb8053036566
Mitchell McCaffrey <mitchemmc@gmail.com>
2022-10-30 08:59:12 +1100

    Add diff check to dice roll sync

--- src/plugin/DiceRollSync.tsx
+++ src/plugin/DiceRollSync.tsx
@@ -1,30 +1,48 @@
 import OBR from "owlbear-rodeo-sdk";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useDiceRollStore } from "../dice/store";
+import { getDieFromDice } from "../helpers/getDieFromDice";
 import { getPluginId } from "./getPluginId";
 
 /** Sync the current dice roll to the plugin */
 export function DiceRollSync() {
-  useEffect(() =>
-    useDiceRollStore.subscribe((state) => {
-      // const values = Object.values(state.rollValues);
-      // if (values.length > 0) {
-      //   if (
-      //     !(
-      //       values.every((value) => value === null) ||
-      //       values.every((value) => value !== null)
-      //     )
-      //   ) {
-      //     return;
-      //   }
-      // }
-      OBR.player.setMetadata({
-        [getPluginId("roll")]: state.roll,
-        [getPluginId("rollThrows")]: state.rollThrows,
-        [getPluginId("rollValues")]: state.rollValues,
-        [getPluginId("rollTransforms")]: state.rollTransforms,
-      });
-    })
+  const prevIds = useRef<string[]>([]);
+  useEffect(
+    () =>
+      useDiceRollStore.subscribe((state) => {
+        let changed = false;
+        if (!state.roll) {
+          changed = true;
+          prevIds.current = [];
+        } else {
+          const ids = getDieFromDice(state.roll).map((die) => die.id);
+          // Check array length for early change check
+          if (prevIds.current.length !== ids.length) {
+            changed = true;
+          }
+          // Check the ids have changed
+          else if (!ids.every((id, index) => id === prevIds.current[index])) {
+            changed = true;
+          }
+          // Check if we'e completed a roll
+          else if (
+            Object.values(state.rollValues).every((value) => value !== null)
+          ) {
+            changed = true;
+          }
+          prevIds.current = ids;
+        }
+
+        if (changed) {
+          OBR.player.setMetadata({
+            [getPluginId("roll")]: state.roll,
+            [getPluginId("rollThrows")]: state.rollThrows,
+            [getPluginId("rollValues")]: state.rollValues,
+            [getPluginId("rollTransforms")]: state.rollTransforms,
+          });
+        }
+      }),
+    []
   );
 
   return null;
