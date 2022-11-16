702d5812dbcd7341c236118f9a2c20ba52c6086c
Mitchell McCaffrey <mitchemmc@gmail.com>
2022-11-16 20:45:34 +1100

    Revert "Update dice store"
    This reverts commit 1eb65181c91e2b87d16ff58c671d8b3bdd572677.

--- src/dice/store.ts
+++ src/dice/store.ts
@@ -29,14 +29,13 @@ interface DiceRollState {
   rollThrows: Record<string, DiceThrow>;
   startRoll: (roll: DiceRoll) => void;
   clearRoll: (ids?: string) => void;
-  rerollAll: () => void;
-  /** Reroll select ids of dice */
-  reroll: (ids: string[]) => void;
+  /** Reroll select ids of dice or reroll all dice by passing `undefined` */
+  reroll: (ids?: string[]) => void;
   finishDieRoll: (id: string, number: number, transform: DiceTransform) => void;
 }
 
 export const useDiceRollStore = create<DiceRollState>()(
-  immer((set, get) => ({
+  immer((set) => ({
     roll: null,
     rollValues: {},
     rollTransforms: {},
@@ -62,23 +61,6 @@ export const useDiceRollStore = create<DiceRollState>()(
         state.rollTransforms = {};
         state.rollThrows = {};
       }),
-    rerollAll: () => {
-      const roll = get().roll;
-      if (roll) {
-        set((state) => {
-          state.roll = roll;
-          state.rollValues = {};
-          state.rollTransforms = {};
-          state.rollThrows = {};
-          const dice = getDieFromDice(roll);
-          for (const die of dice) {
-            state.rollValues[die.id] = null;
-            state.rollTransforms[die.id] = null;
-            state.rollThrows[die.id] = getRandomDiceThrow();
-          }
-        });
-      }
-    },
     reroll: (ids) => {
       set((state) => {
         if (state.roll) {
@@ -104,14 +86,14 @@ export const useDiceRollStore = create<DiceRollState>()(
 /** Recursively update the ids of a draft to reroll dice */
 function rerollDraft(
   diceRoll: WritableDraft<DiceRoll>,
-  ids: string[],
+  ids: string[] | undefined,
   rollValues: WritableDraft<Record<string, number | null>>,
   rollTransforms: WritableDraft<Record<string, DiceTransform | null>>,
   rollThrows: WritableDraft<Record<string, DiceThrow>>
 ) {
   for (let dieOrDice of diceRoll.dice) {
     if (isDie(dieOrDice)) {
-      if (ids.includes(dieOrDice.id)) {
+      if (!ids || ids.includes(dieOrDice.id)) {
         delete rollValues[dieOrDice.id];
         delete rollTransforms[dieOrDice.id];
         delete rollThrows[dieOrDice.id];
