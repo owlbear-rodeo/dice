b422b445e93a45eab135801ee96f52498be36f76
Mitchell McCaffrey <mitchemmc@gmail.com>
2023-07-21 09:44:13 +1000

    Move tray suspense above dice roll button

--- src/tray/TraySuspense.tsx
+++ src/tray/TraySuspense.tsx
@@ -7,7 +7,7 @@ export function TraySuspense({ children }: { children: React.ReactNode }) {
   return (
     <Suspense
       fallback={
-        <Backdrop open sx={{ position: "absolute" }}>
+        <Backdrop open sx={{ position: "absolute", zIndex: 1 }}>
           <CircularProgress size="3rem" />
         </Backdrop>
       }
